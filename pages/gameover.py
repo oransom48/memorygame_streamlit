8580bfd272cba72dd412b4f7c2272452a1e4af06
oompatom <Patommawansuda2547@gmail.com>
2023-11-14 15:29:13 +0700

    add styles + add link font

--- pages/gameover.py
+++ pages/gameover.py
@@ -22,6 +22,16 @@ st.markdown("""
 con = sqlite3.connect('userdb.db')
 cur = con.cursor()
 
+with open("style_gameover.css") as f:
+     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+st.markdown("""<link rel="stylesheet" 
+            href="https://fonts.googleapis.com/css2?family=Silkscreen:wght@400;700&display=swap">"""
+            , unsafe_allow_html=True)
+st.markdown("""<link rel="stylesheet" 
+            href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">"""
+            , unsafe_allow_html=True)
+
 # body
 st.title("Game over :(")
 
--- pages/signup.py
+++ pages/signup.py
@@ -26,6 +26,13 @@ goback = st.button(":arrow_backward: Go back")
 if goback:
     switch_page("main")
 
+with open("style_signup.css") as f:
+     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+st.markdown("""<link rel="stylesheet" 
+            href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">"""
+            , unsafe_allow_html=True)
+
 # main part start here
 st.title("Sign up")
 
