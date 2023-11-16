5dce119bb54995e982053d4e703f2e44a6381431
oransom48 <kornrawee.kochtat@gmail.com>
2023-11-16 11:58:53 +0700

    Add & modify question class

--- pages/game.py
+++ pages/game.py
@@ -28,28 +28,30 @@ if 'keylist' not in st.session_state:
 if 'ans' not in st.session_state:
     st.session_state.ans = ''
 
-# question
-listnum = ['0','1','2','3','4','5','6','7','8','9']
-listchar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
-listmix = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
+class sequence:
+    def __init__(self):
+        # question
+        self.listnum = ['0','1','2','3','4','5','6','7','8','9']
+        self.listchar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
+        self.listmix = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
 
-listchoice = []
-if st.session_state.mode == 'Character':
-    listchoice = listchar
-elif st.session_state.mode == 'Mix it all':
-    listchoice = listmix
-else:
-    listchoice = listnum
+    def choice(self):
+        if st.session_state.mode == 'Character':
+            return self.listchar
+        elif st.session_state.mode == 'Mix it all':
+            return self.listmix
+        else:
+            return self.listnum
 
-def checkstate():
-    if st.session_state.ans == st.session_state.keylist:
-        st.session_state.score += 1
-        st.session_state.keylist = ''
-        for i in range(st.session_state.score + 1):
-            new = random.choice(listchoice)
-            st.session_state.keylist += new
-    else:
-        switch_page("gameover")
+    def check(self):
+        if st.session_state.ans == st.session_state.keylist:
+            st.session_state.score += 1
+            st.session_state.keylist = ''
+            for i in range(st.session_state.score + 1):
+                new = random.choice(self.choice())
+                st.session_state.keylist += new
+        else:
+            switch_page("gameover")
 
 # go to main page
 restart = st.button(":repeat: Restart")
@@ -62,6 +64,8 @@ if restart:
 st.title('Game start!')
 st.write("enter '0' to start game (and get 1 point for :rainbow[free] :smile:)")
 
+question = sequence()
+
 col1, col2 = st.columns(2)
 
 with col2: 
@@ -70,15 +74,18 @@ with col2:
     submit = st.button("Submit")
     if submit:
         del answer
-        checkstate()
+        question.check()
 
 with col1:
-    question = st.session_state.keylist    
-    with st.empty():
-        for i in range(len(question)):
-            st.subheader(f"digit {i+1}: :red[{question[i]}]")
-            time.sleep(1)
-        st.write("Time to answer!")
+    # question = st.session_state.keylist    
+    # with st.empty():
+    #     for i in range(len(question)):
+    #         st.subheader(f"digit {i+1}: :red[{question[i]}]")
+    #         time.sleep(1)
+    #     st.write("Time to answer!")
+    question = st.session_state.keylist
+    st.subheader(f"{question}")
+
 
 st.write(f"Your score: { st.session_state.score }")
     
